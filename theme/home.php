22295798553f22d5bb8355fa50f4a2145f993765
kromoser <kevinromoser@gmail.com>
2020-09-30 09:05:38 -0400

    Initial commit

---
+++ theme/home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying the home page.
+ *
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+
+$context = Timber::context();
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
